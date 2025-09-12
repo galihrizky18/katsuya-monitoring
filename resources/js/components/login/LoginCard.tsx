5f14bd78e5c00d3942df0f4f3de13a5aef5d36cc
Gartix <galihrizkycode@gmail.com>
2025-09-12 10:48:49 +0700

    Card Login

--- resources/js/components/login/LoginCard.tsx
+++ resources/js/components/login/LoginCard.tsx
@@ -1,7 +1,11 @@
 const LoginCard = () => {
     return (
-        <div>
-            <h1>Login Card</h1>
+        <div className="flex flex-col rounded-lg border border-gray-300 bg-white px-10 py-7 drop-shadow-lg">
+            {/* Hedaer */}
+            <div>
+                <h1>Selamat Datang Kembali,</h1>
+                <h2>Di Website Katsuya Monitoring</h2>
+            </div>
         </div>
     );
 };
