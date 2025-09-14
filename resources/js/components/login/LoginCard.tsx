cd9f9623b704ea6f6d1fb6505fa8b211989784b3
garix <katsuyaganurashi@gmail.com>
2025-09-14 17:26:06 +0700

    login page

--- resources/js/components/login/LoginCard.tsx
+++ resources/js/components/login/LoginCard.tsx
@@ -1,15 +1,14 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
-
 import { PasswordInput, TextInput } from '@mantine/core';
-import GlassButton from '../ui/GlassButton';
-import { useState } from 'react';
-import Swal from 'sweetalert2'
 import axios from 'axios';
+import { useState } from 'react';
+import Swal from 'sweetalert2';
+import GlassButton from '../ui/GlassButton';
 
 const LoginCard = () => {
-  const [username, setUsername] = useState('');
+    const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     // const [loading, setLoading] = useState(false);
 
@@ -30,7 +29,7 @@ const LoginCard = () => {
                         'Content-Type': 'application/json',
                         'X-CSRF-TOKEN': csrfToken || '',
                     },
-                }
+                },
             );
 
             const result = response.data; // axios otomatis parsing JSON
@@ -78,11 +77,10 @@ const LoginCard = () => {
         }
     };
 
-
     return (
-        <div className="flex flex-col gap-3 rounded-lg border border-gray-300 bg-white px-10 py-7 font-poppins drop-shadow-lg">
+        <div className="mx-3 flex flex-col gap-3 rounded-xl border border-gray-300 bg-white px-5 font-poppins drop-shadow-lg md:mx-0 md:px-10 md:py-7 py-5">
             {/* Logo */}
-            <div className="flex h-36 items-center justify-center overflow-hidden">
+            <div className="flex h-28 items-center justify-center overflow-hidden md:h-36">
                 <img
                     src="http://101.128.76.179:4071/storage/images/Logo_Katsuya_Ganurashi.png"
                     alt="Logo Katsuya"
@@ -92,20 +90,14 @@ const LoginCard = () => {
 
             {/* Hedaer */}
             <div className="flex flex-col">
-                <h1 className="text-3xl font-bold text-text-default">Selamat Datang Kembali,</h1>
-                <h2 className="text-sm text-gray-500">Di Website Katsuya Monitoring</h2>
+                <h1 className="text-xl md:text-3xl font-bold text-text-default">Selamat Datang Kembali,</h1>
+                <h2 className="text-xs md:text-sm text-gray-500">Di Website Katsuya Monitoring</h2>
             </div>
 
-            <form action="" className="flex flex-col gap-2" onSubmit={handleSubmit}>
-                <TextInput label="Username" placeholder="username"
-                   value={username}
-                    onChange={(e) => setUsername(e.currentTarget.value)}
-                />
+            <form action="" className="flex flex-col gap-2 text-sm md:text-md" onSubmit={handleSubmit}>
+                <TextInput label="Username" placeholder="username" value={username} onChange={(e) => setUsername(e.currentTarget.value)} />
 
-                <PasswordInput label="Password" placeholder="password"
-                     value={password}
-                    onChange={(e) => setPassword(e.currentTarget.value)}
-                />
+                <PasswordInput label="Password" placeholder="password" value={password} onChange={(e) => setPassword(e.currentTarget.value)} />
 
                 <div className="mt-3 flex w-full items-center justify-center">
                     <GlassButton width="50%" type="submit">
@@ -118,4 +110,3 @@ const LoginCard = () => {
 };
 
 export default LoginCard;
-
